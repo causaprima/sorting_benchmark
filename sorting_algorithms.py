9ccf61245e4ac8570650391325fe6fb5ded434fb
arkady_seleznev <a.seleznev@me.com>
2019-10-09 19:50:01 +0700

    small fixes

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -3,7 +3,7 @@ import random
 
 def bubble_sort(unsorted_array):
     a = unsorted_array.copy()
-    for i in range(len(a)):
+    for i in range(len(a)-1):
         for j in range(len(a)-i-1):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
